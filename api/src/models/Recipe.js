3aa555681475d1d466eac7d99e28fd85e94f2075
Ana Fernandes <fernandes.carolina.ana@gmail.com>
2022-02-26 00:17:48 +0000

    try

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,42 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    ID: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true, //esta sera mi clave primaria, o sea, el Id
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary:{
+      type: DataTypes.TEXT,
+      allowNull:false
+    },
+    score:{ //spoonacularScore
+      type: DataTypes.INTEGER,
+    },
+    healthScore:{
+      type: DataTypes.INTEGER
+    },
+    steps:{
+      type: DataTypes.ARRAY(DataTypes.STRING) //confirmar
+      //type: Datatypes.ARRAY(DataTypes.STRING)
+    },
+    readyInMinutes:{
+      type: DataTypes.INTEGER
+    },
+    servings:{
+      type: DataTypes.INTEGER
+    }
+    // dishTypes:{
+
+    // }
+
+    //mirar si quiero adicionar otras columnas:
+    // - Image
+
   });
 };
