0a5ece90c0a3f98220f999825045c8288a4aac36
Ana Fernandes <fernandes.carolina.ana@gmail.com>
2022-03-02 23:55:08 +0000

    route get query?name=...

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -9,59 +9,62 @@ const API_KEY = process.env.API_KEY;
 
 router.get('/', async(req, res)=>{
 
-    let recipe_DB = await Recipe.findAll({
-        include: Diet
-    })
-
-    let recipe_api= await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`);
-
-    //------------ recipes?name="..."
+    //------------ recipes?name="..."-----------------------------------------------------
 
     if(req.query.name){
         try {
 
-            //let recipe_api_filter = recipe_api.
-            
-            // axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&query=${req.body.name}&number=100`);
-
-            // if(!recipe_api){
-            //     return res.status(204).json(`${req.query.name} - Recipe not Found`);
-            // }
-            
-            const recipe_found = recipe_api.data.results.map(r =>{
-                return{
+            let recipes_DB = await Recipe.findAll({
+                include:{
+                    model: Diet,
+                    attributes: ['name'],
+                    through:{
+                        attributes: []
+                    }
+                }
+            })
+        
+            let recipe_api= await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true&number=100`);
+        
+            let api_results = recipe_api.data.results.map(r =>{
+                return {
                     id: r.id,
                     name: r.title,
                     image: r.image,
                     summary: r.summary,
                     score: r.spoonacularScore,
                     healthScore: r.healthScore,
-                    // steps: r.analyzedInstructions[0].steps.map(s => s.step).flat(1).join(""),
+                    steps: r.analyzedInstructions.map(a=> a.steps.map(b=> b.step)).flat(1).join(""),
                     diets: r.diets,
                     dishTypes: r.dishTypes,
                     cuisines: r.cuisines
                 }
             })
-            
-            const total = [...recipe_DB, ... recipe_found];
 
-            const filter = total.filter(r => r.title.toLowerCase().includes(req.query.name.toString().toLowerCase()))
+            const all_recipes = api_results.concat(recipes_DB);
 
-            if(filter.length > 0){
-                return res.json(filter);
-            }else{
-                res.send('Recipe not found')
-            }
+            const resp = [];
 
-            // const filter_recipe_db = recipe_DB.filter(r => r.name.toLowerCase().includes(req.query.name.toLowerCase()))
-            
-            // const result = [...filter_recipe_db, ...recipe_found.splice(0,9)]
+            all_recipes.map(r => { 
+                if(r.name.toLowerCase().includes(req.query.name.toLowerCase())){
+                    resp.push(
+                        {
+                            id: r.id,
+                            name: r.name,
+                            image: r.image,
+                            summary: r.summary,
+                            score: r.score,
+                            healthScore: r.healthScore,
+                            // steps: r.analyzedInstructions.map(a=> a.steps.map(b=> b.step)).flat(1).join(""),
+                            diets: r.diets,
+                            dishTypes: r.dishTypes,
+                            cuisines: r.cuisines
+                        }
+                    )
+                }
+            });
 
-            // // res.json(result);
-
-            // res.json(`${recipe_api.data.results}`)
-
-
+            res.json(resp);
 
         } catch (error) {
             console.log(error)
@@ -103,9 +106,9 @@ router.get('/', async(req, res)=>{
 
 
 
-//-------------- GET BY ID ----------------------------------------------------------
+//-------------- GET BY ID ----------------------------------------------------------------
 
-router.get('/:id', async (req, res, next)=>{
+router.get('/:id', async (req, res)=>{
     let{id} = req.params;
 
     if(id.includes('-')){
