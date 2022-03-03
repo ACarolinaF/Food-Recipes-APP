9207d6bdd30d8f2072f2cf9492ea2b9f59fbb415
Ana Fernandes <fernandes.carolina.ana@gmail.com>
2022-03-03 16:49:47 +0000

    reducer updated - missing order and filter

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,7 +1,18 @@
-import { GET_RECIPES } from "../actions/index.js";
+import { GET_RECIPES,
+        GET_DETAIL_ID,
+        SEARCH_BY_NAME,
+        FILTER_BY,
+        ORDER_BY,
+        POST_RECIPE
+    } from "../actions/index.js";
+
 
 const inicialState={
-    recipes: []
+    recipes: [],
+    recipeById: {},
+    recipesByName: []
+    
+    // ---- TERMINAR O FILTER E O ORDER
 
 }
 
@@ -13,6 +24,26 @@ function rootReducer (state = inicialState , action){
                 ...state,
                 recipes: action.payload
             }
+        case GET_DETAIL_ID:
+            return {
+                ...state,
+                recipeById: action.payload
+            }
+        case SEARCH_BY_NAME:
+            return{
+                ...state,
+                recipesByName: action.payload
+            }
+        case FILTER_BY:
+            return {
+
+            }
+        case ORDER_BY:
+            return{
+
+            }
+        case POST_RECIPE:
+            return{}
         default:
             return state
     }
